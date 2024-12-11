84a1fb7df319eaabc77681be59f9a61be783196d
Nikita Kurguzov <vrolok15@yandex.ru>
2024-12-11 17:51:30 +0300

    Added mouse sprite for food and updated Credits

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,30 @@
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
 
-int cellSize = 30;
-int cellCount = 25;
+int cellSize = 32;
+int cellCount = 24;
 
 class Food{
 
     public:
         Vector2 position = {5, 6};
+        Texture2D texture;
+
+        Food()
+        {
+            Image image = LoadImage("graphics/food.png");
+            texture = LoadTextureFromImage(image);
+            UnloadImage(image);
+        }
+
+        ~Food()
+        {
+            UnloadTexture(texture);
+        }
 
         void Draw()
         {
-            DrawCircle(position.x * cellSize, position.y * cellSize, cellSize / 2, darkGreen);
+            DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
         }
 
 };
