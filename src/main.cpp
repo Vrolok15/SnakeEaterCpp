b2fa447eacaf761c138a059e2fd80184b0d59f94
Nikita Kurguzov <vrolok15@yandex.ru>
2024-12-11 19:00:13 +0300

    Updated body and prevent 180 turn

--- src/main.cpp
+++ src/main.cpp
@@ -113,16 +113,16 @@ int main()
             snake.Update();
         }
 
-        if(IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)){
+        if((IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) && snake.direction.y != 1){
             snake.direction = {0, -1};
         }
-        else if(IsKeyPressed(KEY_RIGHT)|| IsKeyPressed(KEY_D)){
+        else if((IsKeyPressed(KEY_RIGHT)|| IsKeyPressed(KEY_D)) && snake.direction.x != -1){
             snake.direction = {1, 0};
         }
-        else if(IsKeyPressed(KEY_DOWN)|| IsKeyPressed(KEY_S)){
+        else if((IsKeyPressed(KEY_DOWN)|| IsKeyPressed(KEY_S)) && snake.direction.y != -1){
             snake.direction = {0, 1};
         }
-        else if(IsKeyPressed(KEY_LEFT)|| IsKeyPressed(KEY_A)){
+        else if((IsKeyPressed(KEY_LEFT)|| IsKeyPressed(KEY_A)) && snake.direction.x != 1){
             snake.direction = {-1, 0};
         }
         
