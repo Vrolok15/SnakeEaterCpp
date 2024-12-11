6fa6201740b587560b664688c96d1be01d2720bc
Nikita Kurguzov <vrolok15@yandex.ru>
2024-12-11 18:41:21 +0300

    Added snake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <iostream>
+#include <deque>
 
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
@@ -7,7 +8,44 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 32;
 int cellCount = 24;
 
-class Food{
+class Snake
+{
+    public:
+        Texture2D headTexture, bodyTexture;
+        std::deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+
+        Snake()
+        {
+            Image imageHead = LoadImage("graphics/SnakeHead.png");
+            Image imageBody = LoadImage("graphics/SnakeBody.png");
+            headTexture = LoadTextureFromImage(imageHead);
+            bodyTexture = LoadTextureFromImage(imageBody);
+            UnloadImage(imageHead);
+            UnloadImage(imageBody);
+        }
+
+        ~Snake()
+        {
+            UnloadTexture(headTexture);
+            UnloadTexture(bodyTexture);
+        }
+
+        void Draw(){
+            for(unsigned int i = 0; i < body.size(); i++){
+                float x = body[i].x;
+                float y = body[i].y;
+                if(i == 0){
+                    DrawTexture(headTexture, x * cellSize, y * cellSize, WHITE);
+                }
+                else{
+                    DrawTexture(bodyTexture, x * cellSize, y * cellSize, WHITE);
+                }
+            }
+        }
+};
+
+class Food
+{
 
     public:
         Vector2 position;
@@ -47,8 +85,8 @@ int main()
     SetTargetFPS(60);
 
     Food food = Food();
-    Food food2 = Food();
-    Food food3 = Food();
+
+    Snake snake = Snake();
 
     while(WindowShouldClose() == false){
         BeginDrawing();
@@ -56,8 +94,7 @@ int main()
         //Drawing
         ClearBackground(green);
         food.Draw();
-        food2.Draw();
-        food3.Draw();
+        snake.Draw();
 
         EndDrawing();
     }
